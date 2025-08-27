d0a55f0653607197ba855fbbf3fbf8a4b142368f
MasterNemo <info@dragon-software.net>
2025-08-27 12:32:58 +0200

    Added TotalTasks

--- lib/src/features/statistics/widgets/task_counter_card.dart
+++ lib/src/features/statistics/widgets/task_counter_card.dart
@@ -12,6 +12,7 @@ class TaskCounterCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int totalTasks = taskCount + deletedCount;
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
@@ -85,6 +86,45 @@ class TaskCounterCard extends StatelessWidget {
                   const Expanded(
                     child: Text(
                       "Anzahl der gelöschten Tasks",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            color: const Color.fromARGB(26, 222, 103, 255),
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: const BoxDecoration(
+                      color: Colors.redAccent,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Text(
+                      "$totalTasks",
+                      style: const TextStyle(
+                        fontSize: 36,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  const Expanded(
+                    child: Text(
+                      "Gesamtzahl der Tasks",
                       style: TextStyle(
                         fontSize: 18,
                       ),
